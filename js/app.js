e17a989ad64a49cbbfef07d6f909160072b8f02d
OfirNadav <ofir.nadav@gmail.com>
2015-12-07 13:21:17 +0200

    form scss

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
+<!-- Custom Theme JavaScript -->
+
+    // Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+$("[rel='tooltip']").tooltip();
+
+$('.thumbnail').hover(
+    function(){
+        $(this).find('.caption').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+        $(this).find('.caption').slideUp(250); //.fadeOut(205)
+    }
+);
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
